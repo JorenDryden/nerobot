0321b357b4cfd1c4dcbf11ef0eafc27e73bef993
Joren Dryden <joren@sasktel.net>
2024-10-12 08:58:38 -0600

    Initial Commit

---
+++ nerobot.py
@@ -0,0 +1,79 @@
+import discord
+from discord.ext import commands
+import yt_dlp
+import asyncio
+
+intents = discord.Intents.default()
+intents.message_content = True
+intents.voice_states = True
+
+FFMPEG_OPTIONS = {
+    'before_options': '-reconnect 1 -reconnect_streamed 1 -reconnect_delay_max 5', 
+    'options': '-vn'
+}
+
+YDL_OPTIONS = {
+    'format': 'bestaudio', 
+    'noplaylist': 'True'
+}
+
+class MusicBot(commands.Cog):
+    def __init__(self, client):
+        self.client = client
+        self.queue = []
+
+    @commands.command()
+    async def play(self, ctx, *, search):
+        voice_channel = ctx.author.voice.channel if ctx.author.voice else None
+        if not voice_channel:
+            return await ctx.send("## You're not in a voice channel")
+        
+        if not ctx.voice_client:
+            await voice_channel.connect()
+
+        async with ctx.typing():
+            with yt_dlp.YoutubeDL(YDL_OPTIONS) as ydl:
+                info = ydl.extract_info(f"ytsearch:{search}", download=False)
+                if 'entries' in info:
+                    info = info['entries'][0]
+                url = info['url']
+                title = info['title']
+                self.queue.append((url, title))
+                if len(self.queue) > 1:
+                    await ctx.send(f'## Added to queue: **{title}**')                    
+
+    
+            if not ctx.voice_client.is_playing():
+                await self.play_next(ctx)
+
+    async def play_next(self, ctx):
+        if self.queue:
+            url, title = self.queue.pop(0)
+            source = discord.FFmpegPCMAudio(url, **FFMPEG_OPTIONS)
+            ctx.voice_client.play(source, after=lambda _: self.client.loop.create_task(self.play_next(ctx)))
+            await ctx.send(f'## Now Playing: **{title}**')
+
+    @commands.command()
+    async def skip(self, ctx):
+        if ctx.voice_client and ctx.voice_client.is_playing():
+            ctx.voice_client.stop()
+            await ctx.send("## Currently playing song has been skipped.")
+
+    @commands.command()
+    async def queue(self, ctx):
+        if not self.queue:
+            await ctx.send("## NeroBot's queue is currently empty.")
+        else:
+            queue_list = "\n".join([f"{index + 1}. {title}" for index, (_, title) in enumerate(self.queue)])
+            await ctx.send(f"## Current Queue:\n{queue_list}")
+
+    @commands.command()
+    async def commands(self, ctx):
+        await ctx.send(f'## NeroBot Commands\n!play <title> - play a track from youtube\n!skip - skip the current track\n!queue - view the current track queue\n!commands -  display all relevant commands')
+
+client = commands.Bot(command_prefix="!", intents=intents)
+
+async def main():
+    await client.add_cog(MusicBot(client))
+    await client.start('PLACE THE DISCORD TOKEN HERE') # make sure the token is correctly placed between the quotes ('  -->     place here    <--    ')
+asyncio.run(main())
